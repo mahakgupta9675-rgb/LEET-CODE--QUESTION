7bccafaa7bd71cc6c3d84aeec31d948772f2cac4
Mahak Gupta <mahakgupta9675@gmail.com>
2026-06-25 19:48:39 +0530

    Time: 0 ms (100%), Space: 11.8 MB (47.68%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,14 +1,24 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        vector<int>ans;
+        
+        // vector<int>ans;
 
+        // for(int i = 0 ; i <nums.size(); i++){
+        //     if(nums[i]!=val){
+        //         ans.push_back(nums[i]);
+        //     }
+        // }
+        // nums=ans;
+        // return nums.size();
+        int k =0;
         for(int i = 0 ; i <nums.size(); i++){
             if(nums[i]!=val){
-                ans.push_back(nums[i]);
+                swap(nums[k],nums[i]);
+                k++;
             }
-        }
-        nums=ans;
-        return nums.size();
+        } 
+        return k;   
+
     }
 };

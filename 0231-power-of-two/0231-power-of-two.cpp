99a2d2547f0801ac30fa0b1575c4cb6c80710d96
Mahak Gupta <mahakgupta9675@gmail.com>
2026-06-30 00:06:05 +0530

    Time: 0 ms (100%), Space: 7.9 MB (11.98%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,12 +1,18 @@
+// class Solution {
+// public:
+//     bool isPowerOfTwo(int n) {
+//         for(int x=0; x <= 30 ; x++){
+//             int power = pow(2,x);
+//             if(power==n){ 
+//                 return true ;
+//             }
+//         }
+//         return false;
+//     }
+// };
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        for(int x=0; x <= 30 ; x++){
-            int power = pow(2,x);
-            if(power==n){ 
-                return true ;
-            }
-        }
-        return false;
+        return n > 0 && (n & (n - 1)) == 0;
     }
 };
